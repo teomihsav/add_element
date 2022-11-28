38143018764f7c6639d01d1bfcec2cc7594a0137
teomihsav <teomihsav76@gmail.com>
2022-11-29 00:10:12 +0200

    animation added

--- src/components/anim/Anim.js
+++ src/components/anim/Anim.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import logo from '../../assets/header-logo-abalta.svg'
+import logo from '../../assets/logo.png'
 import style from './Anim.module.css'
 
 const Anim = () => {
   return (
-    <div styles={{ marginTop: '5rem', fontSize: '5rem' }}><img className={`${style.rotate} ${style.leftRight}`} src={logo} alt='logo'></img></div>
+    <div styles={{ marginTop: '5rem', fontSize: '5rem' }}><img className={`${style.rotate}`} src={logo} alt='logo'></img></div>
   )
 }
 
